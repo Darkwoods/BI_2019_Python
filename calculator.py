ce064bb786db9738252cae230fee931565af7647
Maria <marija.mamaeva@pharminnoch.com>
2019-09-24 21:44:28 +0300

    Changed code in calculator.py

--- calculator.py
+++ calculator.py
@@ -2,13 +2,13 @@ a = int(input())
 c = input()
 b = int(input())
 if c == '+':
-    print(a+b)
+    print(a + b)
 elif c == '-':
-    print(a-b)
+    print(a - b)
 elif c == '*':
-    print(a*b)
+    print(a * b)
 elif c == '/':
     if b == 0:
         print('Деление на 0 невозможно!')
     else:
-        print(a//b)
+        print(a // b)
